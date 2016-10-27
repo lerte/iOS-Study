78eeda2691a990e6d71a68e56ce2531167c8c914
Lerte Smith <lerte@creativeworks.com.hk>
2016-10-27 10:40:46 +0800

    Initial Commit

---
+++ iOS-Study/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS-Study
+//
+//  Created by Lerte Smith on 2016/10/27.
+//  Copyright © 2016年 Lerte Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
